a8993c84109adf84a5211fa31b0e70d2346544cb
Iain Collins <me@iaincollins.com>
2017-02-24 21:28:10 +0000

    Reduced filesize and dimentions of logo

--- components/header.js
+++ components/header.js
@@ -41,7 +41,7 @@ export default class extends React.Component {
             <div className="row">
               <div className="six columns">
                 <div className="logo">
-                  <Link href="/"><a><img src="/static/images/upsum-logo-2017-02-23.png" alt="Upsum - The news, summed up"/></a></Link>
+                  <Link href="/"><a><img src="/static/images/upsum-logo-2017-02-24.png" alt="Upsum - The news, summed up"/></a></Link>
                 </div>
               </div>
               <div className="six columns">
