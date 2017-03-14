f4374fda78bf66cec29c05787e8616ba10b64d99
Iain Collins <me@iaincollins.com>
2017-03-14 11:32:49 +0000

    Lazy loading images in card previews

--- components/question-card-preview.js
+++ components/question-card-preview.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Router from 'next/router'
 import React from 'react'
 import TimeAgo from 'react-timeago'
+import LazyLoad from 'react-lazy-load'
 
 export default class extends React.Component {
   
@@ -14,8 +15,8 @@ export default class extends React.Component {
       let imageUrl = 'https://res.cloudinary.com/glitch-digital-limited/image/upload/h_600,c_fill/'+fileName
       imageTag = 
         <Link href={"/question?id="+question['@id'].split('/')[4]} as={"/questions/"+question['@id'].split('/')[4]}>
-          <a><div className="question-card-image" style={{backgroundImage: 'url('+imageUrl+')'}}></div></a>
-        </Link>
+          <a className="question-card-image-container"><LazyLoad offsetVertical={300}><div className="question-card-image" style={{backgroundImage: 'url('+imageUrl+')'}}></div></LazyLoad></a>
+        </Link>      
     }
 
     let datePublished = question['@dateModified']
