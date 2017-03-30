461441874c51bcee117e49db2aa8afd80672a3dc
Iain Collins <me@iaincollins.com>
2017-03-30 03:46:34 +0100

    Adding footer copy and tweaking apperance
    * Added information relating to the terms under which images are licenced (i.e. either CC or listed as Public Domain).
    * Improved apperance of footer on desktop and mobile to more clearly seperate it out from main content.

--- components/footer.js
+++ components/footer.js
@@ -20,16 +20,18 @@ export default class extends React.Component {
 
     return (
       <footer>
-        <div className="container">
+        <div className="footer container">
           <div className="row">
             <div className="twelve columns">
-              <hr/>
               <p style={{marginBottom: '10px'}}>
                 &copy; <Link href="/"><a>Upsum</a></Link> {new Date().getYear() + 1900} v{Package.version} {loginButton}
                 <span style={{float: 'right'}} ><a href="/rss.xml"><i className="fa fa-rss"/> RSS</a></span>
               </p>
               <p style={{marginBottom: '10px'}}>
                 Upsum articles are published under the Creative Commons <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0</a> licence.
+              </p>
+              <p style={{marginBottom: '10px'}}>
+                Images from <a href="http://flickr.com">Flickr</a> are licenced under <a href="https://creativecommons.org/">Creative Commons</a> or are Public Domain (see attribution link for details).
               </p>
               <p style={{marginBottom: '10px'}}>
                 Published by <a href="http://glitch.digital">GLITCH.DIGITAL LIMITED</a>, registered in England & Wales as Company number 09942832.
